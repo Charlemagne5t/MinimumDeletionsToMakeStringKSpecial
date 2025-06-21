788c52a88c074c50173eeb256f720d4e05f4d241
alek <wait82985@gmail.com>
2025-06-21 13:33:38 +0500

    initial commit

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -9,35 +9,6 @@ class Solution {
         }
 
         Arrays.sort(freq);
-        int len = 0;
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] != 0) {
-                len++;
-            }
-        }
 
-        int[] nonZero = new int[len];
-        int j = 0;
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] != 0) {
-                nonZero[j++] = freq[i];
-            }
-        }
-        int res = Integer.MAX_VALUE;
-        int countDeletions = 0;
-        for (int i = 0; i < nonZero.length - 1; i++) {
-            int c = 0;
-            for (int l = nonZero.length - 1; l >= i; l--) {
-                if (nonZero[l] - nonZero[i] - k > 0) {
-                    c += nonZero[l] - nonZero[i] - k;
-                }
-            }
-            res = Math.min(res, countDeletions + c);
-            countDeletions += nonZero[i];
-
-        }
-
-
-        return Math.min(countDeletions, res);
     }
 }
